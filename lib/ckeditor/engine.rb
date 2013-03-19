9b2c67480c252ccd860d776060a3e935fc9a3e31
Nikolay Kondratyev <nkondratyev@yandex.ru>
2013-03-19 12:03:32 +0600

    Removed hooks to formtastic and simple_form

--- lib/ckeditor/engine.rb
+++ lib/ckeditor/engine.rb
@@ -15,15 +15,5 @@ module Ckeditor
         ActionView::Helpers::FormBuilder.send :include, Ckeditor::Helpers::FormBuilder
       end
     end
-    
-    initializer "ckeditor.hooks" do
-      if Object.const_defined?("Formtastic")
-        ::Formtastic::SemanticFormBuilder.send :include, Ckeditor::Hooks::FormtasticBuilder
-      end
-      
-      if Object.const_defined?("SimpleForm")
-        ::SimpleForm::FormBuilder.send :include, Ckeditor::Hooks::SimpleFormBuilder
-      end
-    end
   end
 end
